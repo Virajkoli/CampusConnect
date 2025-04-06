c9aa0fc4c638cccbded137de6c4b300ffdd3bafd
PallaviPatil-04 <pallavipatil200404@gmail.com>
2025-04-06 18:58:22 +0530

    profile page upedate

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,10 +1,14 @@
+import { auth } from "../firebase";
+
 function Profile() {
-    return (
-      <div className="flex justify-center mt-10">
-        <h1 className="text-3xl font-bold">Welcome to Profile</h1>
-      </div>
-    );
-  }
-  
-  export default Profile;
-  
+  const user = auth.currentUser;
+
+  return (
+    <div className="flex flex-col items-center mt-10">
+      <h1 className="text-3xl font-bold mb-2">Welcome, {user?.displayName || "User"}</h1>
+      <p className="text-lg">Email: {user?.email}</p>
+    </div>
+  );
+}
+
+export default Profile;
