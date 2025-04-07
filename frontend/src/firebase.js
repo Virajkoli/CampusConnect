cc3afc4799bfae7e346dd09164832b2d9d0143f3
PallaviPatil-04 <pallavipatil200404@gmail.com>
2025-04-07 20:36:37 +0530

    files add and delete

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -23,6 +23,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const firestore = getFirestore(app);
+export const db = getFirestore(app);
 
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
