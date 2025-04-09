dc391994278a427dccae137c0ed5688b4ad9547a
PallaviPatil-04 <pallavipatil200404@gmail.com>
2025-04-09 15:31:25 +0530

    Home Page create

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,16 @@
-function Home() {
+import React from 'react';
+
+const Home = () => {
   return (
-    <div className="flex justify-center mt-10">
-      <h1 className="text-3xl font-bold bg-purple-400">
-        Welcome to CampusConnect
+    <section className="relative bg-gray-700 text-white h-screen flex flex-col justify-center items-center z-0">
+      <h1 className="text-5xl md:text-6xl font-extrabold leading-tight text-center">
+        Connecting Students, Building <br /> Communities
       </h1>
-  
-    </div>
+      <p className="mt-6 text-lg text-center px-6">
+        EduConnect is your one-stop platform to stay updated, connected, and inspired.
+      </p>
+    </section>
   );
-}
+};
 
 export default Home;
