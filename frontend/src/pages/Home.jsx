cba918e15ef8c86b507b55bb213193d163bf8947
PallaviPatil-04 <pallavipatil200404@gmail.com>
2025-04-24 16:30:06 +0530

    .

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,17 +1,9 @@
 function Home() {
   return (
     <section className="relative bg-gray-700 text-white h-screen flex flex-col justify-center items-center z-0">
-<<<<<<< HEAD
-      <div>
-        <h1 className="text-5xl md:text-6xl font-extrabold leading-tight text-center">
-          Connecting Students, Building <br /> Communities
-        </h1>
-      </div>
-=======
       <h1 className="text-5xl md:text-6xl font-extrabold leading-tight text-center">
         Connecting Students, Building <br /> Communities
       </h1>
->>>>>>> ac9e33a27f7c5b00346ab691eca897acfd55702a
     </section>
   );
 }
